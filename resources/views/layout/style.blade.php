0e31ed560496f61acd2364f376ff88b64d7fb441
zkorn2 <91117502+zkorn2@users.noreply.github.com>
2022-09-16 10:01:17 +0700

    add layer

---
+++ resources/views/layout/style.blade.php
@@ -0,0 +1,16 @@
+<meta charset="UTF-8">
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+<title>Asia Thai</title>
+
+<!-- General CSS Files -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+<!-- Template CSS -->
+<link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/components.css')}}">
+<link rel="stylesheet" href="sweetalert2.min.css">
+<!-- Data table -->
+<link rel="stylesheet" href="{{asset('assets/datatables/dataTables.bs4.css')}}" />
+<link rel="stylesheet" href="{{asset('assets/datatables/dataTables.bs4-custom.css')}}" />
+<link href="{{asset('assets/datatables/buttons.bs.css')}}" rel="stylesheet" />
