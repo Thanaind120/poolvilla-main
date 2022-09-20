8addd1afbfe553053fd3c9ffd6e7020e37178258
Thanai Likitpoonsup <thanai.ots2565@gmail.com>
2022-09-20 09:42:38 +0700

    Add Layer

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright &copy; 2022 <div class="bullet"></div>Asia Thai</a>
+    </div>
+    <div class="footer-right">
+        Version 1.0
+    </div>
+</footer>
